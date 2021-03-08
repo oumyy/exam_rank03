19ce1ced44f7e971c092b4080ad9da439f4daa4f
oumyy <56736450+oumyy@users.noreply.github.com>
2021-03-08 11:12:38 +0100

    Update mini_paint.h

--- mini_paint/mini_paint.h
+++ mini_paint/mini_paint.h
@@ -13,17 +13,14 @@ typedef	struct	s_bg
 	int		he;
 	int		wi;
 	char	c;
-	char	space;
 }				t_bg;
 
 typedef	struct s_cir
 {
 	char	type;
-    char    sp1;
 	float	x;
 	float	y;
 	float	rad;
-    char	sp2;
 	char	c;
 }				t_cir;
 
